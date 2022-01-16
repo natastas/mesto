3349819d29d4f420b2173b4e4f9485c4c5cb9334
Stanislav Anuprienko <72942346+natastas@users.noreply.github.com>
2022-01-16 22:41:23 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project: Mesto
+
+This site was created using technology HTML/CSS/JAVASCRIPT
---
+++ js/script.js
@@ -0,0 +1,38 @@
+let popup = document.querySelector('.popup');
+let openPopup = document.querySelector('.button-edit');
+let closePopup = document.querySelector('.popup__close');
+
+openPopup.addEventListener('click', function(events) {
+    events.preventDefault();
+    popup.classList.add('popup_opened');
+  });
+closePopup.addEventListener('click', function(events) {
+    events.preventDefault();
+    popup.classList.remove('popup_opened');
+});
+
+let hearts = document.querySelectorAll('.heart');
+hearts.forEach(function(entry) {
+  entry.onclick = function(event) {
+    event.preventDefault();
+    entry.classList.toggle('heart_active');
+    entry.classList.toggle('heart_notactive');
+  };
+});
+
+
+let formElement = document.querySelector('.popup__body');
+
+function formSubmitHandler (evt) {
+	evt.preventDefault(); 
+
+	let nameInput = document.querySelector('.text_name');
+	let jobInput = document.querySelector('.text_job');
+  let name = document.querySelector('.profile__title');
+  let job = document.querySelector('.profile__subtitle');
+  name.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
