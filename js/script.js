c449351dbde10eaaac64dae608f5e45722744038
Stanislav Anuprienko <72942346+natastas@users.noreply.github.com>
2022-01-19 20:16:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,36 +1,35 @@
 let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.button-edit');
+let openPopup = document.querySelector('.profile__button-edit');
 let closePopup = document.querySelector('.popup__close');
+let name = document.querySelector('.profile__title');
+let job = document.querySelector('.profile__subtitle');
 
-openPopup.addEventListener('click', function(events) {
-    events.preventDefault();
+openPopup.addEventListener('click', function popupOpen() {
     popup.classList.add('popup_opened');
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
   });
-closePopup.addEventListener('click', function(events) {
-    events.preventDefault();
+
+closePopup.addEventListener('click', function popupClose() {
     popup.classList.remove('popup_opened');
 });
-
-let hearts = document.querySelectorAll('.heart');
-hearts.forEach(function(entry) {
-  entry.onclick = function(event) {
-    event.preventDefault();
-    entry.classList.toggle('heart_active');
-    entry.classList.toggle('heart_notactive');
-  };
-});
-
 
 let formElement = document.querySelector('.popup__body');
 
 function formSubmitHandler (evt) {
 	evt.preventDefault(); 
-
-	let name = document.querySelector('.profile__title');
-  let job = document.querySelector('.profile__subtitle');
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
   popup.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+/* let hearts = document.querySelectorAll('.place__heart');
+hearts.forEach(function(entry) {
+  entry.onclick = function(event) {
+    event.preventDefault();
+    entry.classList.toggle('place__heart_active');
+    entry.classList.toggle('place__heart_notactive');
+  };
+}); */
