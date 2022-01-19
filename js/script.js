b0d6f129ed66a4658f080ef1d527a3e695016159
Stanislav Anuprienko <72942346+natastas@users.noreply.github.com>
2022-01-19 22:09:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,25 +3,28 @@ let openPopup = document.querySelector('.profile__button-edit');
 let closePopup = document.querySelector('.popup__close');
 let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__subtitle');
-
-openPopup.addEventListener('click', function popupOpen() {
-    popup.classList.add('popup_opened');
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
-  });
-
-closePopup.addEventListener('click', function popupClose() {
-    popup.classList.remove('popup_opened');
-});
-
 let formElement = document.querySelector('.popup__body');
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+  nameInput.value = name.textContent;
+  jobInput.value = job.textContent;
+};
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+};
 
 function formSubmitHandler (evt) {
 	evt.preventDefault(); 
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  popupClose();
 }
+
+openPopup.addEventListener('click', popupOpen);
+
+closePopup.addEventListener('click', popupClose);
 
 formElement.addEventListener('submit', formSubmitHandler);
 
