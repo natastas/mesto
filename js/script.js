eec156f0592b1232fefc868a139cb606a7737ba4
Anuprienko Stanislav <anuper@yandex.ru>
2022-02-06 14:45:41 +0300

    bug fixed

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const inputLink = document.querySelector('.popup__input-link');
 const inputJob = document.querySelector('.popup__input-job');
 const inputName = document.querySelector('.popup__input-name');
 const formElementAdd = document.querySelector('.popup__body_type_card-add');
-/* const formElementProfile = document.querySelector('.popup__body_type_profile'); */
+const formElementProfile = document.querySelector('.popup__body_type_profile');
 
 initialCards.forEach(renderCard);
 
@@ -88,37 +88,73 @@ function formSubmitHandler(event) {
   popupCloseProfile();
 };
 
-const formElement = document.querySelector('.popup__body_type_profile');
-const formInput = formElement.querySelector('.popup__input-name');
-const formError = formElement.querySelector(`.${formInput.id}-error`);
-// Слушатель события input
-// Функция, которая добавляет класс с ошибкой
-const showInputError = (element) => {
-  element.classList.add('popup__input_type_error');
-  formError.classList.add('popup__input-error_active');
+// Вынесем все необходимые элементы формы в константы
+const formElement = document.querySelector('.popup__body');
+const inputElement = formElement.querySelector('.popup__input');
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  // Находим элемент ошибки внутри самой функции
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // Остальной код такой же
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
 };
-// Функция, которая удаляет класс с ошибкой
-const hideInputError = (element) => {
-  element.classList.remove('popup__input_type_error');
-  formError.classList.remove('popup__input-error_active');
-};
-// Функция, которая проверяет валидность поля
-const isValid = () => {
-  if (!formInput.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formInput);
+
+const hideInputError = (formElement, inputElement) => {
+  // Находим элемент ошибки
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  // Остальной код такой же
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+}; 
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
-    // Если проходит, скроем
-    hideInputError(formInput);
+    hideInputError(formElement, inputElement);
   }
 };
-formElement.addEventListener('submit', function (evt) {
-  // Отменим стандартное поведение по сабмиту
-  evt.preventDefault();
-});
+
+const setEventListeners = (formElement) => {
+  // Находим все поля внутри формы,
+  // сделаем из них массив методом Array.from
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+
+  // Обойдём все элементы полученной коллекции
+  inputList.forEach((inputElement) => {
+    // каждому полю добавим обработчик события input
+    inputElement.addEventListener('input', () => {
+      // Внутри колбэка вызовем isValid,
+      // передав ей форму и проверяемый элемент
+      checkInputValidity(formElement, inputElement)
+    });
+  });
+}; 
+
+const enableValidation = () => {
+  // Найдём все формы с указанным классом в DOM,
+  // сделаем из них массив методом Array.from
+  const formList = Array.from(document.querySelectorAll('.popup__body'));
+
+  // Переберём полученную коллекцию
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      // У каждой формы отменим стандартное поведение
+      evt.preventDefault();
+    });
+
+    // Для каждой формы вызовем функцию setEventListeners,
+    // передав ей элемент формы
+    setEventListeners(formElement);
+  });
+};
+
+// Вызовем функцию
+enableValidation(); 
 // Вызовем функцию isValid на каждый ввод символа
-formInput.addEventListener('input', isValid); 
-console.log(formInput.id);
 
 openPopupProfile.addEventListener('click', popupOpenProfile);
 closePopupImage.addEventListener('click', popupCloseImage);
@@ -126,7 +162,7 @@ closePopupProfile.addEventListener('click', popupCloseProfile);
 openPopupPlace.addEventListener('click', popupOpenPlace);
 closePopupPlace.addEventListener('click', popupClosePlace);
 formElementAdd.addEventListener('submit', addCard);
-/* formElementProfile.addEventListener('submit', formSubmitHandler); */
+formElementProfile.addEventListener('submit', formSubmitHandler);
 
 /* document.addEventListener('keydown', function(ee) {
   if (ee.key === 'Escape') {
